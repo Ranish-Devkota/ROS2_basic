950363a255dfbc77a16dc2de6e3a463ddad2cdd7
Ranish-Devkota <ranish075bei@ioepc.edu.np>
2022-11-12 23:20:51 +0545

    Publsher publishing number in a topic

---
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import Int64
+
+class NumberPublisher(Node): 
+    def __init__(self):
+        super().__init__("number_publisher")
+
+        self.robot_name = "WILLEY"
+        self.publisher_ = self.create_publisher(Int64,"number", 10)
+        self.timer_ = self.create_timer(0.5,self.publish_number)
+        self.get_logger().info("Number_publisher had started with publishing in topic 'number' !!!")
+    
+    def publish_number(self):
+        msg = Int64()
+        msg.data = 35
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = NumberPublisher() 
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
