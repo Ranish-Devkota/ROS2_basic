2532931ac5e2db804bfa82fbe11d1248ff6f79a1
Ranish-Devkota <ranish075bei@ioepc.edu.np>
2022-11-18 00:15:42 +0545

    Costom messages and services

--- src/my_py_pkg/my_py_pkg/template_python_node.py
+++ src/my_py_pkg/my_py_pkg/template_python_node.py
@@ -1,16 +1,14 @@
-
-
 import rclpy
 from rclpy.node import Node
 
-class MyCustomNode(Node): #modify name of class
+class MyCustomNode(Node): # modify_name 
     def __init__(self):
-        super().__init__("node_name")# modify name
+        super().__init__("node_name") # modify_name
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyCustomNode() #modify name
+    node = MyCustomNode() # modify_name
     rclpy.spin(node)
     rclpy.shutdown()
 
